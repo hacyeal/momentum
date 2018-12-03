be547aa724a668e74382c4cfc0291e9a80f3a7b1
hacyeal <hacyeal@naver.com>
2018-12-03 20:00:53 +0900

    making a clock update

--- clock.js
+++ clock.js
@@ -4,13 +4,14 @@ const clockContainer = document.querySelector(".js-clock"),
 function getTime(){
     const date = new Date();
     const hours = date.getHours();
-    const minutes = date.getDate();
+    const minutes = date.getMinutes();
     const seconds = date.getSeconds();
-    clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
+    clockTitle.innerText = `${hours<10 ? `0${hours}` : hours}:${minutes<10 ? `0${minutes}`: minutes}:${seconds<10 ? `0${seconds}`: seconds}`;
 } 
 
 function init(){
     getTime();
+    setInterval(getTime, 1000);
 }
 
 init();
